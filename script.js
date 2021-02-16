aa3fbc1580cbb150fc068bf17a23212d12cc4371
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-02-16 23:48:07 +0530

    game

--- script.js
+++ script.js
@@ -21,10 +21,19 @@ const scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
 
+
+const switchPlayer = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer == 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+}
+
 btnRoll.addEventListener('click',function(){
 
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
+    // console.log(dice);
 
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
@@ -35,13 +44,25 @@ btnRoll.addEventListener('click',function(){
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
     }else{
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer == 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+       switchPlayer();
 
     }
 
+
+});
+
+btnHold.addEventListener('click',function(){
+
+    scores [activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    if(scores[activePlayer] >= 20){
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    }else{
+        switchPlayer();
+    }
+
+  
 
 });
